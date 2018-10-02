eef6e00cfff9aaf5950e4e9a0cba0ab52072a433
Kate <kduncanwelke@gmail.com>
2018-10-02 12:11:59 -0500

    Tweaks, more descriptive comments

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -143,7 +143,7 @@ var noExperience: [[String: String]] = []
 
 for player in players {
         if let playerExperience = player[experience], let boolExp = Bool(playerExperience) {
-        if boolExp == true {
+        if boolExp {
             someExperience.append(player)
         } else {
             noExperience.append(player)
@@ -152,7 +152,7 @@ for player in players {
 }
 
 
-// sort each player group by height, ascending
+// sort each player group by height, ascending, in preparation for height distribution
 
 let sortedExperienced = someExperience.sorted {(left: [String: String], right: [String: String]) -> Bool in
     guard let leftHeight = left[height], let doubledLeft = Double(leftHeight), let rightHeight = right[height], let doubledRight = Double(rightHeight) else {
@@ -167,10 +167,6 @@ let sortedInexperienced = someExperience.sorted {(left: [String: String], right:
 }
 
 
-//let sortedExperienced = someExperience.sorted {($0[height] as? Double) < ($1[height] as? Double)}
-//let sortedInexperienced = noExperience.sorted {($0[height] as? Double) < ($1[height] as? Double)}
-
-
 // initialize empty arrays for teams
 
 var teamSharks: [[String: Any]] = []
@@ -182,15 +178,35 @@ var teamRaptors: [[String: Any]] = []
 
 func dividePlayers(playerGroup: [[String: Any]]) {
 
-    let total = playerGroup.count
-    let thirds = total / 3 // compute how many players fit in each of the three teams
+    let total = playerGroup.count // get total of players
+    let thirds = total / 3 // determine how many players will fit into each team (size of a third)
     
-    // establish ranges for height distribution, divide low-high sorted group into thirds
-    let firstRange = playerGroup[0..<(thirds)] // low range
-    let secondRange = playerGroup[thirds..<(thirds*2)] // medium range
-    let thirdRange = playerGroup[thirds*2..<(thirds*3)] // high range
+    /* establish ranges for height distribution
+       divide groups of players (which were sorted previously from low to high height)
+       into three height ranges - low, medium, and high, using calculated size of a third to break them up evenly */
+    let firstThird = thirds
+    let secondThird = thirds * 2
+    let thirdThird = thirds * 3
     
-    // distribute low height range across teams
+    let firstRange = playerGroup[0..<(firstThird)] // low range
+    let secondRange = playerGroup[firstThird..<(secondThird)] // medium range
+    let thirdRange = playerGroup[secondThird..<(thirdThird)] // high range
+    
+    /* EXPLANATION of the below
+        this code iterates through each height range, assigning lowest and highest heights to the same team
+        i is used to represent index, and assign each number in turn, computed count-1 prevents loop from going out of bounds
+     
+        process looks like this  [1,2,2] [3,4,5] [6,6,7]
+                                  ^       ^           ^    first group receives lowest and highest values
+                                    ^       ^       ^      second group receives next value (+1 index for 1st and 2nd group, -1 for 3rd)
+                                      ^       ^   ^        then third group
+     
+        resulting in these group assignments: (1,3,7) - average 3.6 (2,4,6) - average 4 (2,5,6) - average 4.3
+        these numbers have an average within a 1.5 range of each other
+        an unconventional approach, but based on what worked on paper, and it works with different number sets
+    */
+    
+    // distribute low height range across teams, using logic explained above
     for _ in 1..<firstRange.count-1 {
         var i = 0
         teamSharks.append(firstRange[i])
@@ -201,7 +217,7 @@ func dividePlayers(playerGroup: [[String: Any]]) {
         i += 1
     }
     
-    // distribute medium height range across teams
+    // distribute medium height range across teams, using logic explained above
     for _ in 1..<secondRange.count-1 {
         var i = thirds
         teamSharks.append(secondRange[i])
@@ -212,9 +228,9 @@ func dividePlayers(playerGroup: [[String: Any]]) {
         i += 1
     }
 
-    // distribute high height range across teams
+    // distribute high height range across teams, using logic explained above
     for _ in 1..<thirdRange.count-1 {
-        var i = thirdRange.count*thirds - 1
+        var i = thirdRange.count*thirds - 1 // this is used to start counting from the last index value
         teamSharks.append(thirdRange[i])
         i -= 1
         teamDragons.append(thirdRange[i])
@@ -237,13 +253,13 @@ func averageHeight(for team: [[String: Any]]) -> Double {
     
     for player in team {
         if let playerHeight = player[height] as? Double {
-        totalHeight += playerHeight
+        totalHeight += playerHeight // add all player heights together
         }
     }
     
-    let teamCount = Double(team.count)
+    let teamCount = Double(team.count) // get total number of players
     
-    let averageHeight = totalHeight / teamCount
+    let averageHeight = totalHeight / teamCount // divide total height by number of players
     return averageHeight
 }
 
@@ -268,7 +284,7 @@ func createLetters(team: [[String: Any]], teamName: String, practiceDate: String
         let letter = "Dear \(playerGuardian), \nYour child, \(playerName), has been placed on the \(teamName) team for the soccor league. The \(teamName) have their first practice set for \(practiceDate), so please be certain that your child attends, has the proper equipment, and brings along lots of energy. It is very important that \(name) attend the practice, so be sure to mark the date and time on your calendars! \nWe look forward to seeing you there!\n"
         letters.append(letter)
         }
-    }
+    }   
 }
 
 // call function to create letters
